5e63a6325172e53045bce70f07311cfa1ad031ad
onyoo <roberto9m9@gmail.com>
2015-12-05 14:51:00 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,44 @@ require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
-  
+  def initialize(name,grade,id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    sql = <<-sql
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-sql
+      DROP TAble students
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-sql
+      INSERT INTO students (name,grade)
+      VALUES (?,?)
+    sql
+    DB[:conn].execute(sql,name,grade)
+  end
+
+  def self.create(name:,grade:)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
 end
