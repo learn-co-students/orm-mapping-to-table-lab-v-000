45a7b6f3a4213d2c44e9eff82da9a9f0183830d1
azheng249 <andy.zheng249@gmail.com>
2016-01-23 18:22:26 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,35 @@
+require "sqlite3"
+
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
   
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT);")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students;")
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?);", self.name, self.grade)
+    @id = DB[:conn].execute("SELECT id FROM students WHERE name = ? AND grade = ?", self.name, self.grade).flatten[0]
+  end
+
+  def self.create(attributes)
+    student = Student.new(nil, nil)
+    attributes.each { |attr, value| student.send("#{attr}=", value)}
+
+    student.save
+    student
+  end
+
 end
