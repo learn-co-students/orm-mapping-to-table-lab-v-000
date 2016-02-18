acccd0a9444411c2c299c8439ddb1066f07d8b59
zachnewburgh <zach.newburgh@gmail.com>
2016-02-18 14:28:50 -0500

    remove multiline SQL in .drop_table, clarified use of [0][0] in #save

--- lib/student.rb
+++ lib/student.rb
@@ -22,19 +22,17 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-      SQL
+    sql = "DROP TABLE IF EXISTS students;"
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
-      VALUES (?, ?)
+      VALUES (?, ?);
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0] #[0][0] == join.to_i
   end
 
   def self.create(name:, grade:)
