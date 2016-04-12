6c5b9a4123b790b5405cedc57e8a696221a6c7ae
lukegrecki <lukegrecki@gmail.com>
2016-04-12 19:54:26 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,8 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.create(student_hash)
-    student = Student.new(student_hash[:name], student_hash[:grade])
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
     student.save
     student
   end
