c41814365d40b945329f26a2b6091a7cc608f5ef
Jonathan Stone <jon_p_stone@yahoo.com>
2016-06-19 05:06:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,35 +3,35 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id=nil)
-    @id = some_id
+    @id = id
     @name = name
     @grade = grade
   end
 
   def self.create_table
     sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
         )
-      SQL
+    SQL
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE IF EXISTS students
-    SQL
+    sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
+
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create(name:, grade:)
@@ -39,5 +39,4 @@ class Student
     student.save
     student
   end
-
 end
