15c7fca65d46d0dd9079dde5c1eca62b706a3c94
Adam Taitano <adam.taitano@gmail.com>
2016-07-18 14:11:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,41 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-HEREDOC
+    CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade TEXT
+    );
+    HEREDOC
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(attributes)
+    student = Student.new(attributes.each {|key, value| self.send(("#{key}="), value)})
+    student.save
+    student
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", @name, @grade)
+    @id = DB[:conn].execute("SELECT id FROM students WHERE id = (SELECT MAX(id) FROM students)")
+  end
+
 end
