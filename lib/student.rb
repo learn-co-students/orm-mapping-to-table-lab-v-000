abf5faf1101744bf63a38f597c3678f9901c17f9
Jesse Novotny <jessenovotny@gmail.com>
2016-07-25 17:33:21 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -31,11 +31,10 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-    binding.pry
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql,self.name,self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
