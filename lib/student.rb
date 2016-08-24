d9e82b93981aafd731d4678c56edaaac571c3c9d
Perla Yates <pianistps@yahoo.com>
2016-08-24 23:31:41 +0000

    added table and drop table methods

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,28 @@ class Student
   attr_reader :id
 
   def initalize(name, grade, id=nil)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER);
+        SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+     SQL
+
+     DB[:conn].execute(sql)
   end
 
 end
