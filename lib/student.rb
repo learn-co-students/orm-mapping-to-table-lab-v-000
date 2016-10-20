09cbb70b05ded8d4daf0ce40ff2b5d6adcd882bf
Elyse Klova <elyse.klova@gmail.com>
2016-10-20 01:29:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(student_hash)
-    student = self.new(student_hash[:name], student_hash[:grade])
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
     student.save
     student
   end
