c55bf203b00c6168e4ac77128ef4496f8fd7f9c4
bharti gurnaney <bharti0717@yahoo.com>
2016-11-10 21:32:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 class Student
-attr_accessor :name, :grade, :id
+attr_accessor :name, :grade
 def initialize(name,grade,id=nil)
   @name=name
   @grade=grade
@@ -28,7 +28,7 @@ sql = <<-SQL
  VALUES (?,?)
  SQL
  DB[:conn].execute(sql, self.name, self.grade)
- @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+ id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
 
  def self.create(studenthash)
