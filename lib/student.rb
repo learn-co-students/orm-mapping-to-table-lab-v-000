4538d3e70bf5d503dc5a4a2abf243c9476b4e9d1
bharti gurnaney <bharti0717@yahoo.com>
2016-11-10 21:34:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ sql = <<-SQL
  VALUES (?,?)
  SQL
  DB[:conn].execute(sql, self.name, self.grade)
- id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+ @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
 
  def self.create(studenthash)
