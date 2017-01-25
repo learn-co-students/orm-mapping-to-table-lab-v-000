74062b381074340c1b4354e1f02558771f3bd1fb
Jonathan Foster <redcabinny@gmail.com>
2017-01-25 03:49:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -36,8 +36,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end
 
-  def self.create(student)
-    student = self.new(student[:name], student[:grade])
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
     student.save
     student
   end
