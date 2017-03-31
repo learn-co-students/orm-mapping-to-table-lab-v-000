2817db8cb4222137ec6d4c6baa5a218dae34e22b
David Lewitan <dovidlwtn@gmail.com>
2017-03-31 16:51:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,27 +9,28 @@ class Student
     end
 
     def self.create_table
-      sql = <<-SQLite3
-        CREATE TABLE students (
+      sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
           id INTEGER PRIMARY KEY,
           name TEXT,
           grade INTEGER
         );
-      SQLite3
+      SQL
       DB[:conn].execute(sql)
     end
 
     def self.drop_table
-      sql = <<-SQLite3
-        DROP TABLE students;
-      SQLite3
+      sql = <<-SQL
+        DROP TABLE IF EXISTS students;
+      SQL
       DB[:conn].execute(sql)
     end
 
     def save
-      sql = <<-SQLite3
-        INSERT INTO students (name, grade) VALUES (?, ?);
-      SQLite3
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?);
+      SQL
       DB[:conn].execute(sql, @name, @grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
