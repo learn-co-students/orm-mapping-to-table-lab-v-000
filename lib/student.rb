2adb1fb83da96111daf905af83e98f59b8f656b6
David Lewitan <dovidlwtn@gmail.com>
2017-03-31 16:49:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,42 @@
 class Student
+    attr_accessor :name, :grade
+    attr_reader :id
+    
+    def initialize(name, grade, id = nil)
+      @name = name
+      @grade = grade
+      @id = id
+    end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+    def self.create_table
+      sql = <<-SQLite3
+        CREATE TABLE students (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade INTEGER
+        );
+      SQLite3
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = <<-SQLite3
+        DROP TABLE students;
+      SQLite3
+      DB[:conn].execute(sql)
+    end
+
+    def save
+      sql = <<-SQLite3
+        INSERT INTO students (name, grade) VALUES (?, ?);
+      SQLite3
+      DB[:conn].execute(sql, @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+
+    def self.create(options = {name: nil, grade: nil})
+      new_student = self.new(options[:name], options[:grade])
+      new_student.save
+      new_student
+    end
 end
