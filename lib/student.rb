6fe617c0572e655fc84ea9848a78eda53fe8372a
charlie lee <ctlee9096@gmail.com>
2017-06-12 21:39:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,15 @@
 class Student
-#Student` class
-# 'name` and a `grade'
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
-
+  #attr_writer :id
   def initialize(name, grade, id=nil)
   	@id = id
   	@name = name
   	@grade = grade
   end
- #1. Write a class that is mapped, or equated, to a database table.
- #2. Build a method that creates a table that maps to the given class.
+
   def self.create_table
   	sql = <<-SQL
   	 CREATE TABLE IF NOT EXISTS students (
@@ -38,16 +35,11 @@ class Student
   	VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-   end
-### The `.create` Method
-#This is a class method that uses keyword arguments.
-#The keyword arguments are `name:` and `grade:`.
-#Use the values of these keyword arguments to:
-#1) instantiate a new `Student` object with `Student.new(name, grade)`
-#and 2) save that new student object
-#via `student.save`. The `#create` method should return the student object that it creates.
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
   def self.create(name:, grade:)
-  	student = Students.new(name, grade)
+  	student = Student.new(name, grade)
   	student.save
   	student
   end
