95d2f7ef3993b9c38acecd14b5a1a5153be704c5
gb23 <gregbenj@hotmail.com>
2017-06-17 12:48:09 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,52 @@
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn] 
+  def initialize(name=nil, grade=nil, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end 
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+     DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES(?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT MAX(id) FROM students").join.to_i
+  end
+
+  def self.create(attributes)
+    student = Student.new()
+    attributes.each{ |key, value|
+      student.send("#{key}=", value)
+    }
+    # Student.new(name, grade).tap { |student|
+    #   student.save
+    # }
+    student.save
+    student
+  end
   
 end
