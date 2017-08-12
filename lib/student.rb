b55f50fa6335aa747f48d40c9b0ee261054eeba1
jonathan <jonathanyi81@gmail.com>
2017-08-12 19:54:19 -0400

    finished save method

--- lib/student.rb
+++ lib/student.rb
@@ -31,4 +31,15 @@ class Student
     DB[:conn].execute(sql)
   end # drop_table
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end # save
+
 end
