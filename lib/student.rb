2d81ba7348f8ff8d16c52ea7a6273fad45cff9c7
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 16:58:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,14 @@ class Student
     @id = id
   end
 
-  def self.save
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create_table
