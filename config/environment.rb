408c4f987145141b45d12bccb1908780135b7460
Richard Burd <richardburdx@yahoo.com>
2017-10-05 23:46:10 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ Bundler.require
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+# DB[:conn].execute("CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY, name TEXT, grade TEXT)")
