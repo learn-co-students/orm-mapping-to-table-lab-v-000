c70e06ad2963655e9cc063d775e7da8c0a0ff800
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-09 23:27:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,7 @@ class Student
     @grade = grade
     @id = id
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+
 
   def self.create_table
     sql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)"
