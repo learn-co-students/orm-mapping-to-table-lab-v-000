1be5932e52bc66a8443651c996378b4cd5402f9f
Carol Greene <carolgreene3@gmail.com>
2018-03-10 22:39:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -22,18 +22,17 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL
-    DROP TABLE students
-    SQL
+    sql = "DROP TABLE students"
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-    INSERT INTO students (name, grade) VALUES (?, ?)
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
     SQL
+    
     DB[:conn].execute(sql, self.name, self.grade)
-
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0]
   end
 
