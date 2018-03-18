eca9d9ec689f9da95c2b0bf170d487d658ea455a
Shangni Hu <shangnihu@gmail.com>
2018-03-18 18:22:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,7 @@ class Student
           grade TEXT
         );
         SQL
+        
     DB[:conn].execute(sql)
   end
 
@@ -25,6 +26,7 @@ class Student
     sql = <<-SQL
       DROP TABLE students;
     SQL
+
     DB[:conn].execute(sql)
   end
 
@@ -32,6 +34,7 @@ class Student
     sql = <<-SQL
       INSERT INTO students (name, grade) VALUES (?,?);
     SQL
+
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
