f6e4f073937e8a89c439a7c7cf8490497f915dfe
sgibson47 <sgibson47@gmail.com>
2018-06-14 14:33:21 -0500

    add #self.create_table

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,15 @@ class Student
     @grade = grade
     @id = id
   end
+
+  def self.create_table
+    sql = <<- SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
   
 end
