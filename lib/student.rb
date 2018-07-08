d2312c997fcb33c6d08f447da8dbdd50cd4011d4
Barbara Choy <barbchoy@gmail.com>
2018-07-08 21:11:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,8 +34,8 @@ class Student
      @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")[0][0]
   end
 
-  def self.create
-
+  def self.create(name, grade)
+    
   end
 
 end
