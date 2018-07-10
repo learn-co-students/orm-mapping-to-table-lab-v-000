e717d754148ae950bb472ee18cf0324ab7add6de
Dale Terrell <tragicid@gmail.com>
2018-07-10 20:14:27 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 class Student
  
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
  
   def initialize(name, grade, id=nil)
     @id = id
@@ -29,6 +30,12 @@ class Student
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+    
  
   def self.create(name:, grade:)
     student = Student.new(name, grade)
