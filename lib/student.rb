cfb695a500f3136df8039a1dd0434aa7b32c24df
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 18:56:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,54 @@
-class Student
+require 'rspec'
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    self.name = name
+    self.grade = grade
+    @id = id
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT MAX(id) FROM students;")[0][0]
+
+  end
+
+
+  # CLASS METHODS
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER);
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(attributes)
+    attrs = attributes.collect {|k, v| v}
+    student = Student.new(attrs[0], attrs[1])
+    student.save
+    student
+  end
+
 end
