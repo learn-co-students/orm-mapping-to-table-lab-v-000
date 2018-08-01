fedc47562cf6aae4968c869e4286fbb024e27f19
Dani Schuhman <dani8439@aol.com>
2018-08-01 18:06:45 +0000

    second pass

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,17 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+
   attr_accessor :name, :grade
   attr_reader :id
+
+  @@all = []
 
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
+    @@all << self
   end
 
   def self.create_table
@@ -16,32 +20,33 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade TEXT
+        grade INTEGER
       )
       SQL
-    DB[:conn].execute(sql)
+      DB[:conn].execute(sql)
   end
 
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
+    def self.drop_table
+      sql = <<-SQL
+        DROP TABLE IF EXISTS students
+      SQL
+      DB[:conn].execute(sql)
+    end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
+    def save
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
 
-  def self.create(name: , grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
-
+    def self.create(name: , grade:)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
 end
