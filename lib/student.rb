8211785c5ac2d89b337539f5c5ba91a6563d924c
Ashley Wong <ashwong04@gmail.com>
2019-01-06 00:02:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,23 +13,28 @@ class Student
   end
 
   def self.create_table
-    sql = <<- SQL
-     CREATE TABLE IF NOT EXISTS students(
-       id INTEGER PRIMARY KEY, 
-       name TEXT, 
-       grade TEXT)
+    sql = <<-SQL 
+     CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
       SQL
     DB[:conn].execute(sql) 
   end
 
   def self.drop_table
-    DB[:conn].execute("DROP TABLE students")
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
   end
 
-  def save
-    DB[:conn].execute("
-    INSERT INTO students (name, grade) VALUES (?, ?)",self.name, self.grade)
-    
+  def save(name, grade)
+    sql = <<-SQL
+     INSERT INTO students (name, grade) VALUES (?, ?)",
+     name, grade)
+     SQL
+    DB[:conn].execute(sql)
+    new_id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")
+    self.id = new_id
   end
 
 
