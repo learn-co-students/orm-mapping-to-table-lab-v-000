8c759a33bd2ada261d6c78fda0c174e4ecf63ada
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:09:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,4 @@
 class Student
-
   attr_accessor :name, :grade
   attr_reader :id
 
@@ -9,38 +8,40 @@ class Student
     @id = id
   end
 
-def self.create_table
-  sql = <<-SQL
-  CREATE TABLE students (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER
-  )
-  SQL
-  DB[:conn].execute(sql)
-end
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
 
-def self.drop_table
-  sql = <<-SQL
-  DROP TABLE students;
-  SQL
+    DB[:conn].execute(sql)
+  end
 
-  DB[:conn].execute(sql)
-end
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students;
+    SQL
 
-def save
-  sql = <<-SQL
-  INSERT INTO students (name, grade)
-  VALUES (?, ?);
-  SQL
+    DB[:conn].execute(sql)
+  end
 
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-end
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
 
-def self.create(name:, grade:)
-  student = self.new(name, grade)
-  student.save
-  student
-end
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
