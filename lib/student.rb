831997751cad2dde9416f8dc0e6d0c50757f54ca
Sharon Rieger <sharonarieger@gmail.com>
2019-02-12 13:47:51 -0500

    create a save and create a create method for our instances

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,32 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def drop_table
-    
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE students
+        SQL
+    DB[:conn].execute(sql)
   end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
+  end
+
+  # we don't want to automatically save every instance of a song when it's initialize so call this method when we need to do just that.
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
