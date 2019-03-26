83ffa8a93230ff1fe2af1c30ac043410c7acaeb0
Amil Masic <amil.masic@gmail.com>
2019-03-26 08:49:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,13 @@
 class Student
 
+  attr_accessor :student, :grade, :id
+
+  def initialize (student, grade, id = nil)
+    @student = student
+    @grade = grade
+    @id = id
+  end
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
