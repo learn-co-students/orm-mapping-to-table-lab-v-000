fb13a755e4263171c70dd959858f38624ae348e5
pmknyc <pmknyc@optimum.net>
2020-07-05 18:25:21 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -38,9 +38,8 @@ class Student
       SQL
       
     DB[:conn].execute(sql, self.name, self.grade)
-    p @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  binding.pry
+  #   binding.pry
   end
 
  # .create
